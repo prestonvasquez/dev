340c1a94aa01d2521153ebf416849e094f75e867
Preston Vasquez <prestonvasquez@icloud.com>
2024-02-15 15:47:57 -0700

    Add script to setup RHEL VM for testing

---
+++ scripts/mongodb/setup-rhel.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 
+# Export env variables required to test on a RHEL VM.
+
+GO_VERSION="go1.20"
+
+git clone git@github.com:prestonvasquez/mongo-go-driver.git
+
+export PATH="$PATH:/opt/golang/$GO_VERSION/bin"
+export GOROOT="/opt/golang/$GO_VERSION"
