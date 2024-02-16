f269d87458437b6db1380f1f182b8606ac92f6bc
Preston Vasquez <prestonvasquez@icloud.com>
2024-02-15 19:59:27 -0700

    Update windows

--- scripts/mongodb/setup-rhel.sh
+++ scripts/mongodb/setup-rhel.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
-# 
-# Export env variables required to test on a RHEL VM.
-
-GO_VERSION="go1.20"
 
 # Move to the root director.
-cd
+cd $HOME
 
 git clone git@github.com:prestonvasquez/mongo-go-driver.git
+git clone git@github.com:mongodb-labs/drivers-evergreen-tools.git
+
+export MONGO_ORCHESTRATION_HOME=$HOME
+export STPATH=$HOME/mongo-go-driver
+export DRIVERS_TOOLS=$HOME/drivers-evergreen-tools
+export PROJECT_DIRECTORY=$HOME/mongo-go-driver
+export MONGO_ORCHESTRATION_HOME=$DRIVERS_TOOLS/.evergreen/orchestration
+
+# Remove the mongodb dir if it already exists in driver tools 
+rm -rf $DRIVERS_TOOLS/mongodb
+
+# Start the server on 27017
+sh $DRIVERS_TOOLS/.evergreen/run-orchestration.sh
+sudo netstat -tuln | grep 27017
+
+echo "export the following: 
+GO_VERSION="go1.20"
 
 export PATH="$PATH:/opt/golang/$GO_VERSION/bin"
 export GOROOT="/opt/golang/$GO_VERSION"
+"
--- scripts/mongodb/setup-windows.sh
+++ scripts/mongodb/setup-windows.sh
@@ -2,11 +2,37 @@
 # 
 # Export env variables required to test on a windows VM.
 
-GO_VERSION="go1.20"
+git clone git@github.com:prestonvasquez/mongo-go-driver.git
+
+
+# Move to the root director.
+cd $HOME
 
 git clone git@github.com:prestonvasquez/mongo-go-driver.git
+git clone git@github.com:mongodb-labs/drivers-evergreen-tools.git
+
+export MONGO_ORCHESTRATION_HOME=$(cygpath -m $HOME)
+export STPATH=$(cygpath $HOME/mongo-go-driver)
+export DRIVERS_TOOLS=$(cygpath $HOME/drivers-evergreen-tools)
+export PROJECT_DIRECTORY=$(cygpath $HOME/mongo-go-driver)
+export MONGO_ORCHESTRATION_HOME=$(cygpath $DRIVERS_TOOLS/.evergreen/orchestration)
+export VENV_BIN_DIR=$(cygpath $DRIVERS_TOOLS/.evergreen/orchestration/venv/Scripts)
+export PYTHONIOENCODING=UTF-8
+
+choco upgrade python
+
+# Remove the mongodb dir if it already exists in driver tools 
+rm -rf $DRIVERS_TOOLS/mongodb
+
+# Start the server on 27017
+sh $DRIVERS_TOOLS/.evergreen/run-orchestration.sh
+sudo netstat -tuln | grep 27017
+
+echo "export the following: 
+GO_VERSION="go1.20"
 
 export GOROOT="C:\\golang\\$GO_VERSION"
 export PATH="$GOROOT/bin:$PATH"
 export GOPATH="C:\\home\\Administrator\\mongo-go-driver"
 export GOCACHE="C:\\home\\Administrator\\mongo-go-driver\\mongo\\integration\\unified\\.cache"
+"
