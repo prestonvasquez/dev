8e091197dc624f43487c2efa9f7366eb7bd1bb03
Preston Vasquez <prestonvasquez@icloud.com>
2025-02-11 14:55:37 -0700

    reading from closed con

---
+++ standard-library/net/reading_from_closed_conn.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"time"
+)
+
+func main() {
+	// Create a TCP listener on a random available port
+	ln, err := net.Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		panic(err)
+	}
+	defer ln.Close()
+
+	// Get the listener's address
+	addr := ln.Addr().String()
+	fmt.Println("Listening on:", addr)
+
+	// Start a goroutine to accept a connection
+	go func() {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Accept error:", err)
+			return
+		}
+		fmt.Println("Client connected")
+
+		// Close the connection after a short delay
+		time.Sleep(1 * time.Second)
+		conn.Close()
+		fmt.Println("Server closed connection")
+	}()
+
+	// Dial the server
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+
+	// Wait for the server to close the connection
+	time.Sleep(2 * time.Second)
+
+	// Try to read from the closed connection
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Read error:", err) // Expect "use of closed network connection"
+	} else {
+		fmt.Println("Read", n, "bytes:", string(buf[:n]))
+	}
+}
